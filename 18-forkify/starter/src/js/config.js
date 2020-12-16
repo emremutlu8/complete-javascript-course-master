ed522ef66c8d1bdadd32319984f91b4b80419f1d
Emre MUTLU <mutluemre93@gmail.com>
2020-12-16 13:44:59 +0300

    All js

---
+++ 18-forkify/starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2fad79df-fb44-4962-b52c-b667af3400ea';
+export const MODAL_CLOSE_SEC = '2.5';
